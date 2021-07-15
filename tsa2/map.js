49731fb378bbeaf19022fd38925b18473b817bd1
pncl <anormalgecko@gmail.com>
2021-07-15 12:46:16 -0700

    [TSA2] spawn voids now spawn enemies
    ooooops

--- tsa2/map.js
+++ tsa2/map.js
@@ -164,8 +164,9 @@ function makeWalls(){
           walls.push(new Wall(w*gameMap.w/gameMap.xDivisions, h*gameMap.h/gameMap.yDivisions,gameMap.w/gameMap.xDivisions,gameMap.h/gameMap.yDivisions,{r: 120, g: 90, b: 0}, 10, 500, 0.5));
           break;
         case 15:
-          //spawn void (a wall that the player cannot spawn in)
+          //spawn void (a wall that the player cannot spawn in, but there is still an enemy spawner)
           walls.push(new Wall(w*gameMap.w/gameMap.xDivisions, h*gameMap.h/gameMap.yDivisions,gameMap.w/gameMap.xDivisions,gameMap.h/gameMap.yDivisions,{r: 120, g: 90, b: 0}, 10, 500, 0.5, [], 50, 50, 1, [], [], true));
+          spawners.push(new Spawner(w*gameMap.w/gameMap.xDivisions, h*gameMap.h/gameMap.yDivisions,gameMap.w/gameMap.xDivisions,gameMap.h/gameMap.yDivisions));
         default:
           break;
       }
